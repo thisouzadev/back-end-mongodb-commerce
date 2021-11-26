02f810b155e18a66791c631b1a03947ce97f74f5
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2021-11-26 18:08:00 -0300

    desafio 3

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -3,4 +3,4 @@ db.produtos.updateMany(
   { $set: { valorUnitario: NumberDecimal("0.00") } },
 );
 
-db.produtos.find({}, { _id:0, valorUnitario: 1, nome: 1 });
+db.produtos.find({}, { _id: 0, valorUnitario: 1, nome: 1 });
