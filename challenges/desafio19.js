5255ea799d8852e5961bf5bb54acd1c3fb49fcae
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2021-11-26 22:51:33 -0300

    desafio 21

--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -2,9 +2,10 @@ db.produtos.updateMany(
   {},
   {
     $rename: {
-      "descricao": "descricaoSite",
+      descricao: "descricaoSite",
     },
   });
+
 db.produtos.find(
   {},
   { _id: 0, nome: 1, descricaoSite: 1 },
